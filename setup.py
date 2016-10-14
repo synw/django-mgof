05adbe43a0069b0b9ae9fe804be2969e98d701f7
syn <synwe@yahoo.fr>
2016-10-14 13:36:44 +0000

    Setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+version = __import__('mgof').__version__
+
+setup(
+  name = 'django-mgof',
+  packages=find_packages(),
+  include_package_data=True,
+  version = version,
+  description = 'Modern good old forums for Django',
+  author = 'synw',
+  author_email = 'synwe@yahoo.com',
+  url = 'https://github.com/synw/django-mgof', 
+  download_url = 'https://github.com/synw/django-mgof/releases/tag/'+version, 
+  keywords = ['django', 'forum'], 
+  classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Framework :: Django :: 1.9',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+  zip_safe=False,
+  install_requires=[
+        'django-braces',
+        'bleach',
+        'django-ckeditor',
+        'django-mqueue',
+    ],
+)
